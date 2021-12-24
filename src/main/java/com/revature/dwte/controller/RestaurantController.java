35e7dad70c86b85e47467b7d3b00891ab94aeb0f
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2021-12-24 13:26:18 -0600

    remove unused imports

--- src/main/java/com/revature/dwte/controller/RestaurantController.java
+++ src/main/java/com/revature/dwte/controller/RestaurantController.java
@@ -1,6 +1,5 @@
 package com.revature.dwte.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.dwte.annotation.AdminAndMember;
 import com.revature.dwte.exception.InvalidParameterException;
 import com.revature.dwte.model.Restaurant;
-import com.revature.dwte.model.Review;
 import com.revature.dwte.model.User;
 import com.revature.dwte.service.RestaurantService;
 import com.revature.dwte.utility.ValidateUtil;

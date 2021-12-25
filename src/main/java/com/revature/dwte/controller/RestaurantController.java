e2d11aecd4dd42aa70745a219eb110a0905f078e
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2021-12-25 10:13:03 -0500

    created getRestaurantByRestaurantId endpoint

--- src/main/java/com/revature/dwte/controller/RestaurantController.java
+++ src/main/java/com/revature/dwte/controller/RestaurantController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +53,7 @@ public class RestaurantController {
 	}
 
 	@GetMapping(path = "/restaurant")
-	public ResponseEntity<Object> getRestaurant(@RequestBody Map<String, String> json)
+	public ResponseEntity<Object> getRestaurantByRestaurantNameAndAddress(@RequestBody Map<String, String> json)
 			throws InvalidParameterException {
 		logger.info("RestaurantController.getRestaurantId() invoked");
 
@@ -64,6 +65,26 @@ public class RestaurantController {
 					json.get("restaurantName").trim(), json.get("restaurantAddress").trim());
 
 			return ResponseEntity.status(200).body(restaurantId);
+		} catch (NoResultException e) {
+			return ResponseEntity.status(400).body(e.getMessage());
+
+		} catch (InvalidParameterException e) {
+			return ResponseEntity.status(400).body(e.getMessage());
+
+		}
+	}
+
+	@GetMapping(path = "/restaruant/{restaurantId}")
+	public ResponseEntity<Object> getRestaruatnByRestaurantId(@PathVariable int restaurantId)
+			throws InvalidParameterException {
+		logger.info("RestaurantController.getRestaruatnByRestaurantId() invoked");
+
+		try {
+			validateUtil.verifyRestaurantId(restaurantId);
+
+			Restaurant restaurant = restaurantService.getRestaurantByRestaurantId(restaurantId);
+
+			return ResponseEntity.status(200).body(restaurant);
 		} catch (NoResultException e) {
 			return ResponseEntity.status(400).body(e.getMessage());
 
